cc94a3f5237019d74a80a2c9d72a9a53c769e59c
Wesley Medeiros <devwesleymedeiros@yahoo.com>
2025-01-09 11:41:45 -0300

    Exercício TS 0012 inserido

---
+++ exercicios_TS/EX0012/index.js
@@ -0,0 +1,53 @@
+"use strict";
+class BankingAccount {
+    saldo;
+    constructor(saldo) {
+        this.saldo = saldo;
+    }
+    // Getter para o saldo
+    get getBalance() {
+        return this.saldo;
+    }
+    // Setter para novo depósito
+    set setNewDeposit(novoSaldo) {
+        try {
+            if (novoSaldo > 0) {
+                this.saldo += novoSaldo;
+                console.log(`Seu saldo atual é de ${this.saldo}`);
+            }
+            else {
+                throw new Error("O valor do depósito deve ser maior que zero");
+            }
+        }
+        catch (error) {
+            console.error(error.message);
+        }
+        finally {
+            console.log("Operação de depósito finalizada");
+        }
+    }
+    // Método de retirada
+    withdraw(retirada) {
+        try {
+            if (retirada <= 0) {
+                throw new Error("O valor da retirada deve ser maior que zero");
+            }
+            if (retirada > this.saldo) {
+                throw new Error("Saldo insuficiente");
+            }
+            this.saldo -= retirada;
+        }
+        catch (error) {
+            console.error(error.message);
+        }
+        finally {
+            console.log("Operação de retirada finalizada");
+        }
+    }
+}
+const myAccount = new BankingAccount(1518);
+console.log("Meu saldo inicial: " + myAccount.getBalance);
+myAccount.setNewDeposit = 1000;
+console.log("Meu saldo inicial mais o depósito: " + myAccount.getBalance);
+myAccount.withdraw(500);
+console.log("Meu saldo após a retirada: " + myAccount.getBalance);
